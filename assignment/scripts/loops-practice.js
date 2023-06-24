a35a63f4a89437d3d1366f6429581aecbfc63141
Gabe Glasco <gabeglasco@gmail.com>
2023-06-24 13:59:06 -0500

    fixed 3a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -48,26 +48,29 @@ for ( star of stars ){
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
+console.log('Some stars using while:');
 let i = 0
 let text = ''
 while( stars[i] ){
+  console.log( stars[i] );
   text += stars[i];
   i++;
 }
-console.log('Some stars using while:', stars );
- 
 
+ 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
+console.log('count from 0 to 5:');
 let index = 0;
 let max = 6
 while( index < max ){
-console.log('count from 0 to 5:', index );
-index++;
+  console.log( index );
+  index++;
 }
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
+console.log('count backwards from 10 to 5:');
 index = 10
 let min = 4
 while( index > min ){
-  console.log('count backwards from 10 to 5:', index );
+  console.log( index );
   index--;
 }
